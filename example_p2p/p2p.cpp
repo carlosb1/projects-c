e14a182d5e2b55481c2ae56806aad900f7ecb068
Carlos B <carlos.baezruiz@gmail.com>
2019-01-12 17:04:19 +0100

    Finished routing protocol for chord

--- example_p2p/p2p.cpp
+++ example_p2p/p2p.cpp
@@ -7,25 +7,30 @@
 #include <utility>
 #include <bitset>
 #include <limits>
+#include <algorithm>
 
 
 class Node {
 	public: 
 		std::size_t my_id;
-		std::vector<std::size_t> finger_table;
+		std::vector<std::size_t> finger_table{std::vector<std::size_t>(32, 0)};
 		std::size_t predecessor_id;
 		std::string name;
 		Node(const std::string & name) {
 			this->name = name;
 			this->my_id = std::hash<std::string>{}(name);
 			this->predecessor_id = 0;
-			this->finger_table.push_back(0);
 	
 		}
 		Node() {
 			this->my_id = 0;
 			this->predecessor_id = 0;
-			this->finger_table.push_back(0);
+		}
+		Node(std::size_t possible_id) {
+			this->name = std::string();
+			this->my_id = possible_id;
+			this->predecessor_id = 0;
+
 		}
 	
 		inline std::size_t getSuccessorId() const {
@@ -35,14 +40,7 @@ class Node {
 			this->finger_table[0] = successor_id;
 		}
 		//TODO CHANGE PLACE FOR THIS FUNCTION
-		void set_up_finger_table(const std::unordered_map<std::size_t, std::shared_ptr<Node>> & nodes, const std::size_t last_node_id) {
-			this->finger_table.clear();
-			auto id_to_analyse = this->my_id;
-			for (int i=0; i < 32; i++) {
-				id_to_analyse = (id_to_analyse << 1) % (std::numeric_limits<unsigned long>::max());
-				this->finger_table.push_back(id_to_analyse);	
-			}
-		}
+
 	
 };
 
@@ -68,13 +66,9 @@ class App {
 		inline std::unordered_map<std::size_t,std::shared_ptr<Node>> getNodes()  {
 			return this->nodes;
 		}
-		App() {
-		
-		};
-		void join(const Node & node) {
-			auto node_to_save = std::make_shared<Node>(node);	
-			std::cout <<"##############################\n";
-			if (nodes.find(node.my_id) != nodes.end()) {
+		App() {};
+		void join(std::shared_ptr<Node> node_to_save) {
+			if (nodes.find(node_to_save->my_id) != nodes.end()) {
 					std::clog << "It was added the node\n";
 					return;
 			}
@@ -84,8 +78,7 @@ class App {
 			}
 			if (!this->first.get()) {
 				this->first = node_to_save;
-				this->nodes[node.my_id] = node_to_save;
-				std::clog << "new node  :"<< *node_to_save  << "\n";
+				this->nodes[node_to_save->my_id] = node_to_save;
 				return;
 			}
 
@@ -113,61 +106,78 @@ class App {
 				node_to_save->setSuccessorId(possible_successor->my_id);	
 			}
 			
-			std::clog << "new node  :"<< *node_to_save  << "\n";
-			this->nodes[node.my_id] = node_to_save;
+			this->nodes[node_to_save->my_id] = node_to_save;
 		}
 
 		std::pair<bool, std::shared_ptr<Node>> search(std::shared_ptr<Node> & node_to_search) {
-			auto id_to_search = (*node_to_search).my_id;
-			
+			auto id_to_search = (*node_to_search).my_id;	
 			auto node_to_compare = this->first;
-			while (node_to_compare->my_id < node_to_search->my_id) {
-					if (node_to_compare->getSuccessorId() == 0) {
-						 break;
-					}
-					node_to_compare = calculate_next_node_to_search(node_to_search, node_to_compare); 
+
+			//TODO change order!
+			while ( node_to_search->my_id >= node_to_compare->my_id ) {
+				if (node_to_compare->getSuccessorId() == 0) {
+					 break;
+				}
+				node_to_compare = calculate_next_node_to_search(node_to_search, node_to_compare); 
 			}
-			if (node_to_compare->my_id < node_to_search->my_id) {
+			if ( node_to_search->my_id >= node_to_compare->my_id ) {
 				return std::make_pair(false, node_to_compare);
 			} else {
 				return std::make_pair(true, node_to_compare);
 			}
 		}
 
-		std::shared_ptr<Node> calculate_next_node_to_search(std::shared_ptr<Node> & node_to_search, std::shared_ptr<Node> & node_to_compare) {
-			/*
-			for (int i = node_to_compare->finger_table.size(); i >= 0; i--) {
-				if (node_to_search <= node_to_compare->finger_table[i]) {
-					return this->nodes[node_to_compare->finger_table[i]];
-				}
+		inline int distance(std::size_t id_to_search, std::size_t id_to_compare) {
+			return std::abs((int)(id_to_search -  id_to_compare));	
+		}
 
+		std::shared_ptr<Node> calculate_next_node_to_search(std::shared_ptr<Node> & node_to_search, std::shared_ptr<Node> & node_to_compare) {
+			auto near = this->nodes[node_to_compare->getSuccessorId()];
+
+			for (int i = 0; i<node_to_compare->finger_table.size(); i++) {
+				//TODO check where we find same node
+				if (node_to_search->my_id < node_to_compare->finger_table[i])  {
+					int distance_to_compare = distance(node_to_search->my_id, node_to_compare->finger_table[1]);
+					int current_distance = distance(node_to_search->my_id, near->my_id);
+					if (distance_to_compare < current_distance) {
+						near = this->nodes[node_to_compare->finger_table[i]];	
+					}
+					//return this->nodes[node_to_compare->finger_table[i]];	
+				}
 			}
-			return node_to_search;
-			*/
-			return this->nodes[node_to_compare->getSuccessorId()];
+			return 	near;
+		}
+
+		void set_up_finger_table(std::shared_ptr<Node> & node_to_update) {
+			std::fill(node_to_update->finger_table.begin()+1, node_to_update->finger_table.end(),0);
+			auto id_to_analyse = node_to_update->my_id;
+			for (int i=1; i < 32; i++) {
+				//id_to_analyse = (id_to_analyse << 1) % (std::numeric_limits<unsigned long>::max());
+				//id_to_analyse = (((node_to_update->my_id % this->last_node_id) + 1 ) << i);
+				//TODO add search!!
+				id_to_analyse = (node_to_update->my_id + (2 << (i - 1))) %  this->last_node_id;
+
+				auto node_to_search = std::make_shared<Node>(id_to_analyse);
+				std::pair<bool,std::shared_ptr<Node>> found_node =  this->search(node_to_search);
+				std::clog << "node id="<<i << " id_to_analyse="<<id_to_analyse<<" found_node=" <<found_node.second->my_id<<"\n";
+				node_to_update->finger_table[i] = found_node.second->my_id;
+			}
 		}
 };	
 
 int main () {
-	Node node("hello world");
-	Node node2("hello world1");
-	Node node3("hello world2");
-	Node node4("hello world3");
-	Node node5("hello world4");
+	std::shared_ptr<Node> node = std::make_shared<Node>(1);
+	std::shared_ptr<Node> node2 = std::make_shared<Node>(5);
+	std::shared_ptr<Node> node3 = std::make_shared<Node>(10);
+	std::shared_ptr<Node> node4 = std::make_shared<Node>(50);
+	std::shared_ptr<Node> node5 = std::make_shared<Node>(20);
 	App app;
-	app.join(node);	
+	app.join(node);
 	app.join(node2);	
 	app.join(node3);	
-	app.join(node4);	
-	app.join(node5);	
+	app.join(node4);
+	app.join(node5);
 	
-	node.set_up_finger_table(app.getNodes(), app.getLastNodeId());
-	node2.set_up_finger_table(app.getNodes(), app.getLastNodeId());
-	node3.set_up_finger_table(app.getNodes(), app.getLastNodeId());
-	node4.set_up_finger_table(app.getNodes(), app.getLastNodeId());
-	node5.set_up_finger_table(app.getNodes(), app.getLastNodeId());
-
-	/*
 	std::cout <<"@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
 	auto toNode = app.getFirst();
 	bool exit = false;
@@ -179,6 +189,16 @@ int main () {
 		}
 		toNode = app.getNodes()[toNode->getSuccessorId()];
 	}
-	*/
 
+
+	std::cout << "Added node1= "<< *node <<"\n";
+	app.set_up_finger_table(node);	
+	std::cout << "Added node2= "<< *node2 <<"\n";
+	app.set_up_finger_table(node2);
+	std::cout << "Added node3= "<< *node3 <<"\n";
+	app.set_up_finger_table(node3);
+	std::cout << "Added node4= "<< *node4 <<"\n";
+	app.set_up_finger_table(node4);	
+	std::cout << "Added node5= "<< *node5 <<"\n";
+	app.set_up_finger_table(node5);
 }
